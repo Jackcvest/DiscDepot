3f2443d1a5addf1b9e68ff1e54484769f5b748cb
jcvest <jvest3@illinois.edu>
2026-06-09 23:40:28 -0500

    learning models

--- depotApp/models.py
+++ depotApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class artist(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class song(models.Model):
+    title = models.CharField(max_length=128)
+    artists = models.CharField(max_length=128)
+
+class album(models.Model):
+    title = models.CharField(max_length=128)
+    
