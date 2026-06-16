a475818977b7f82de54496b13416728514d562ac
jcvest <jvest3@illinois.edu>
2026-06-15 21:27:32 -0500

    more robust models for song/album objects

--- DiscDepot/urls.py
+++ DiscDepot/urls.py
@@ -17,10 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from depotApp import views
-
 urlpatterns = [
-    path("", views.testPage, name="home"),
-    path("depotApp/", include("depotApp.urls")),
     path('admin/', admin.site.urls),
+    path("", include("depotApp.urls"))
 ]
